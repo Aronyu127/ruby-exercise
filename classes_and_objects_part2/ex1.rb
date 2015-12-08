b0d845388707649d8f4eb0ad886a696d4f7633e7
Aron Yu <h0935674938@hotmail.com>
2015-12-08 12:34:38 +0800

    ex2

--- classes_and_objects_part2/ex1.rb
+++ classes_and_objects_part2/ex1.rb
@@ -2,9 +2,19 @@ class MyCar
     def self.gas_mileage(gallons, miles)
     puts "#{miles / gallons} miles per gallon of gas"
     end
+    attr_accessor:color,:year,:model
+    def to_s
+       puts "my car is #{color} color,#{year} year #{model} model"
+    end
 end
 
 
 
 
+
 MyCar.gas_mileage(20,250)
+newcar=MyCar.new
+newcar.color="blue"
+newcar.year="2015"
+newcar.model="sharp"
+newcar.to_s
