f303612f7a024179118da8f21e4887f7371005f1
Aron Yu <h0935674938@hotmail.com>
2015-12-09 09:57:35 +0800

    ex1

--- classes_and_objects_part2/ex1.rb
+++ classes_and_objects_part2/ex1.rb
@@ -1,4 +1,4 @@
-class MyCar
+class Vehicle
     def self.gas_mileage(gallons, miles)
     puts "#{miles / gallons} miles per gallon of gas"
     end
@@ -12,9 +12,24 @@ end
 
 
 
-MyCar.gas_mileage(20,250)
-newcar=MyCar.new
+Vehicle.gas_mileage(20,250)
+
+
+
+newcar=Vehicle.new
 newcar.color="blue"
 newcar.year="2015"
 newcar.model="sharp"
 newcar.to_s
+
+class MyCar < Vehicle
+ car_price = 10000
+end 
+
+
+class MyTruck < Vehicle
+ car_price = 20000
+  
+
+
+end	
